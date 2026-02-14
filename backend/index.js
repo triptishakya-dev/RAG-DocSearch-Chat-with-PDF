4b1e63eec4f23b2d0762bba5790b21e795841dbd
triptishakya-dev <triptishakya.2001@gmail.com>
2026-02-15 00:44:35 +0530

    Added the project structure in backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.get("/api/test" , (req,res)=> {
+return res.json("health is good")
+})
+
+app.listen(PORT , ()=>{
+    console.log("server is listening")
+})
+
